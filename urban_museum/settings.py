40adff2e41c84d9c57577e0704e2c0528fe45daa
dcvela23 <dcvela23@outlook.es>
2017-04-03 17:47:06 +0100

    responsive

--- urban_museum/settings.py
+++ urban_museum/settings.py
@@ -141,11 +141,11 @@ EMAIL_USE_TLS = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
